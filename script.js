dfec8d66647371fc728569751a3d0ce9b7012e72
Thecoolcoder <79680268+3030years@users.noreply.github.com>
2022-03-24 17:27:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // air
+function json(url) {
+  return fetch(url).then(res => res.json());
+}
+
+let apiKey = 'your_api_key';
+json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+  console.log(data.ip);
+  console.log(data.city);
+  console.log(data.country_code);
+  // so many more properties
+});
